3adeb52d08a9b9f373437b4a4a8aec365f18c8b7
Greg Patterson <pattersonga@gmail.com>
2016-11-02 17:35:53 -0600

    Updated to meet class requirements.

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -8,19 +8,42 @@ LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
   $scope.lunchMessage = "";
   $scope.numLunchItems = 0;
-  $scope.lunchMenu = "";
+  $scope.fontColor = "black";
+  $scope.lunchMenuBorderColor = "black";
+  $scope.lunchMenu = "list comma separated dishes you usually have for lunch";
 
   $scope.checkLunchBox = function () {
-    getItemCount($scope.lunchMenu);
-    if ($scope.numLunchItems == 0){
+    console.log("Checking lunch box!")
+    console.log("$scope.lunchMenu");
+    console.log($scope.lunchMenu);
+    if ($scope.lunchMenu == ""){
       $scope.lunchMessage = "Please enter data first";
+      $scope.lunchMenuBorderColor = "red";
+      $scope.fontColor = "red";
+      return;
     }
+
+    getItemCount($scope.lunchMenu);
+    if ($scope.numLunchItems <= 3 ){
+      $scope.lunchMessage = "Enjoy!";
+      $scope.lunchMenuBorderColor = "green";
+      $scope.fontColor = "green";
+    } else if ($scope.numLunchItems > 3 ){
+      $scope.lunchMessage = "Too much!";
+      $scope.lunchMenuBorderColor = "green";
+      $scope.fontColor = "green";
+    }
+
   };
 
   function getItemCount(lunchBox) {
-    var re = /\s*;\s*/;
-    var lunchList = lunchBox.split(re);
+    var re = "/\s*;\s*/";
+    var lunchList = lunchBox.split(",");
     $scope.numLunchItems = lunchList.length;
+    console.log("lunchList: ")
+    console.log(lunchList);
+    console.log("NumLunchItems");
+    console.log($scope.numLunchItems);
   };
 }
 
